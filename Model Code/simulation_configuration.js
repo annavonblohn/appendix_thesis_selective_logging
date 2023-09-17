400457aa93a317b8614f17507f7f0f69e5f12b56
annavonblohn <72494744+annavonblohn@users.noreply.github.com>
2023-09-17 14:01:56 +0200

    Create simulation_configuration.js
    Configuration for simulation runs. Definition of model version, number of patches per grid cell ( npatch * (endgrid - startgrid)), definition of simulation start and end, definition of shuffle climate

---
+++ Model Code/simulation_configuration.js
@@ -0,0 +1,60 @@
+
+/**  I.   Simulation description and type section                                  \n**/
+/**  II.  Input parameter section                                                  \n**/
+/**  III. Input data section                                                       \n**/
+/**  IV.  Output data section                                                      \n**/
+/**  V.   Run settings section                                                     \n**/
+/**                                                                                \n**/
+/** (C) Potsdam Institute for Climate Impact Research (PIK), see COPYRIGHT file    \n**/
+/** authors, and contributors see AUTHORS file                                     \n**/
+/** This file is part of LPJmL and licensed under GNU AGPL Version 3               \n**/
+/** or later. See LICENSE file or go to http://www.gnu.org/licenses/               \n**/
+/** Contact: https://github.com/PIK-LPJmL/LPJmL                                    \n**/
+/**                                                                                \n**/
+/**************************************************************************************/
+
+//#define DAILY_OUTPUT  /* enables daily output */
+
+{   /* LPJmL configuration in JSON format */
+
+/*===================================================================*/
+/*  I. Simulation description and type section                       */
+/*===================================================================*/
+
+  "sim_name" : "LPJmL Run", /* Simulation description */
+  "sim_id"   : "lpjml",     /* LPJML Simulation type with managed land use */
+  "version"  : "5.6",       /* LPJmL version expected */
+  "coupled_model": null,
+  "individual" : true,
+  "npatch" : 4,
+  "isD95max" : true,
+  "root_model": "logistic",  /* root growth model (linear, logistic, none) */
+  "cut_year": -9999,     /* year where all trees are logged */
+  "tree_year": -1,       /* year when trees can establish again after cut_year*/
+  "random_prec" : true,     /* Random weather generator for precipitation enabled */
+  "random_seed" : 2,        /* seed for random number generator */
+  "radiation" : "radiation",/* other options: "cloudiness", "radiation", "radiation_swonly", "radiation_lwdown" */
+  "fire" : "no_fire",          /* fire disturbance enabled, other options: NO_FIRE, FIRE, SPITFIRE, SPITFIRE_TMAX (for GLDAS input data) */
+  "fire_on_grassland" : false, /* enable fire on grassland for Spitfire */
+  "fdi" : "nesterov",       /* different fire danger index formulations: "wvpd" (needs GLDAS input data), "nesterov" */
+  "firewood" : false,
+  "new_phenology" : true,   /* GSI phenology enabled */
+  "new_trf" : false,        /* new transpiration reduction function disabled */
+  "river_routing" : false,
+  "extflow" : false,
+  "permafrost" : true,
+  "johansen" : true,
+  "logging_wooddensity": {"low": 2.2e5, "high": 3.1e5},
+  "soilpar_option" : "no_fixed_soilpar", /* other options "no_fixed_soilpar", "fixed_soilpar", "prescribed_soilpar" */
+  "with_nitrogen" : "no", /* other options: "no", "lim", "unlim" */
+  "store_climate" : true, /* store climate data in spin-up phase */
+  "const_climate" : false,
+  "shuffle_climate" : true, /* shuffle spinup climate */
+  "const_deposition" : false,
+  "depos_year_const" : 1901,
+  "fix_climate" : true,
+  "fix_climate_year" : 1996,
+  "fix_climate_cycle" : 30,
+  "fix_landuse" : false,
+#ifdef FROM_RESTART
+  "logging" : true,
